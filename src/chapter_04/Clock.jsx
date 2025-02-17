ff03227eba6383f77a158c8ec6adcec4709b57b3
Junsik Bang <jbangdev@gmail.com>
2025-02-18 02:01:55 +0900

    Update Clock.jsx

--- src/chapter_04/Clock.jsx
+++ src/chapter_04/Clock.jsx
@@ -1,10 +1,11 @@
-﻿import React from "react"
+import React from "react"
 
 function Clock() {
     return (
         <div>
             <h1>안녕, 리액트!</h1>
             <h2>현재 시간 : {new Date().toLocaleTimeString()}</h2>
+            <h3>Have a nice day!</h3>
         </div>
     );
 }
