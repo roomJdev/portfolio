fd8fdcdc6f3fc6457101c60a20f81aadf192500f
kakicream <jbangdev@gmail.com>
2025-02-18 17:21:48 +0900

    upload test

--- src/chapter_04/Clock.jsx
+++ src/chapter_04/Clock.jsx
@@ -9,7 +9,7 @@ function Clock() {
             <h4>Heyheyhey</h4>
             <h5>gogogogo</h5>
             <h6>wowowowowowoowowow</h6>
-            <h3>Why? </h3>
+            <h3>Why? Not working?</h3>
         </div>
     );
 }
